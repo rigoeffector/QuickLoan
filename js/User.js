01be9b4dd68ccffabf2022204c830837f851b72e
rigoeffector <rigorene48@gmail.com>
2019-04-27 14:16:09 -0700

    All updates files

---
+++ js/User.js
@@ -0,0 +1,27 @@
+var toggleButton = document.querySelector(".toggle-button");    
+var mobilenav = document.querySelector(".mobile-nav");
+var backdrop = document.querySelector(".backdrop");
+
+
+toggleButton.addEventListener('click',()=>{
+    mobilenav.style.display='block';
+    backdrop.style.display='block';
+  
+});
+
+backdrop.addEventListener('click',()=>{
+    mobilenav.style.display ='none';
+    closeMobileNav();
+})
+
+
+// backdrop.addEventListener('click',()=>{
+//     mobilenav.style.display='none';
+// })
+    const closeMobileNav = () =>{
+        backdrop.style.display='none';
+        
+    }
+
+
+    
